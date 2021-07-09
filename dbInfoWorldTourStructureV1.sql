b6dcc325900c9a19aa793d4e76c6bea43b3c9be8
Thibcott <thibcott@outlook.com>
2021-07-09 12:34:45 +0200

    add db structure

---
+++ dbInfoWorldTourStructureV1.sql
@@ -0,0 +1,36 @@
+
+create database dbInfoWorldTour;
+use dbInfoWorldTour;
+
+CREATE TABLE IF NOT EXISTS `dbInfoWorldTour`.`tblvoyage` (
+  `voyId` INT(11) NOT NULL AUTO_INCREMENT,
+  `voyData` longtext NULL DEFAULT NULL,
+  `voyUser` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`voyId`));
+  
+
+CREATE TABLE IF NOT EXISTS `dbInfoWorldTour`.`tblmessage` (
+  `mesId` INT(11) NOT NULL AUTO_INCREMENT,
+  `mesText` LONGTEXT NULL DEFAULT NULL,
+  `mesUser` VARCHAR(255) NULL DEFAULT NULL,
+  `mesDate` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`mesId`));
+
+
+INSERT INTO tblVoyage (voyData, voyUser)
+VALUES (
+'{   
+    "Ville":"",
+    "Pays":"",
+    "HeureLocal":"",
+    "NomHebergement":"",
+    "TelHebergement":"",
+    "DateArriver":"",
+    "DateDepart":"",
+    "NbrJours":"",
+    "Divers":""
+}',
+'ExmpleUser');
+
+insert INTO tblMessage(mesText,mesUser,mesDate)
+values("creation de la db v1","DBInsertUser",GETDATE());
